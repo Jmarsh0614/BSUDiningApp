51e34102e9b816772f79f87d6e7b8066cdffa626
Joshua Marshall <jmarsh0614@gmail.com>
2021-10-19 11:36:55 -0400

    10/19/2021
    Saved current HTML files as PHP for future processing

---
+++ Calica.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html><!--Begin Page-->
+<html>
+	<link rel="stylesheet" href="style.css"><!--Add Style Sheet Link-->
+	<head>
+		<title>Bowie State Dining Hall Portal</title>
+		<div class="topnav"><!--Top Nav Bar Code-->
+			<a href="C:\Users\Joshua\Documents\GitHub\BSUDiningPortal\Index.html">Home</a>
+			<a href="C:\Users\Joshua\Documents\GitHub\BSUDiningPortal\HomeSkillet.html">Home Skillet</a>
+			<a href="C:\Users\Joshua\Documents\GitHub\BSUDiningPortal\Calica.html">Calica Burrito</a>
+			<a href="C:\Users\Joshua\Documents\GitHub\BSUDiningPortal\BulldogBites.html">BulldogBites</a>
+			<a href="#Contact">Contact</a>
+		</div>
+		
+		
+	</head>
+
+	<!--Begin Body-->
+    <body>  
+
+		<img src="C:\Users\Joshua\Documents\Web Dev\COSC465Project\ChilacaBanner.JPG">
+
+		<h3>Hours</h3>
+		<img src="C:\Users\Joshua\Documents\Web Dev\COSC465Project\ChilacaHours.JPG" class="hours-pics" alt="Hours">
+		<hr>
+	
+		<!--Begin Burrito Section-->
+		<div>
+
+			<p class="special-p">Burrtio</p>
+			<p>$6.75</p>
+
+			<br>
+
+			<p class="special-p">Bowl</p>
+			<p>$6.75</p>
+
+			<br>
+
+			<p class="special-p">Salad</p>
+			<p>$6.75</p>
+
+			<br>
+
+			<p class="special-p">(2) Tacos</p>
+			<p>$2.55</p>
+
+		</div>
+		<!--End Burrtio Section-->
+
+
+
+
+
+
+    </body>
+	<!--End Body-->
+
+    </html><!--End Page-->
---
+++ Index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html><!--Begin Page-->
+<html>
+	<link rel="stylesheet" href="style.css"><!--Add Style Sheet Link-->
+	<head>
+		<title>Bowie State Dining Hall Portal</title>
+		<div class="topnav"><!--Top Nav Bar Code-->
+			<a href="C:\Users\Joshua\Documents\GitHub\BSUDiningPortal\Index.html">Home</a>
+			<a href="C:\Users\Joshua\Documents\GitHub\BSUDiningPortal\HomeSkillet.html">Home Skillet</a>
+			<a href="C:\Users\Joshua\Documents\GitHub\BSUDiningPortal\Calica.html">Calica Burrito</a>
+			<a href="C:\Users\Joshua\Documents\GitHub\BSUDiningPortal\BulldogBites.html">BulldogBites</a>
+			<a href="#Contact">Contact</a>
+		</div>
+		
+		
+	</head>
+
+<body><!--Begin Body-->
+
+
+
+<h1 id="home">Bowie State Dining Hall Portal</h1>
+
+
+
+<a href="C:\Users\Joshua\Documents\GitHub\BSUDiningPortal\HomeSkillet.html">
+	<img src="C:\Users\Joshua\Documents\Web Dev\COSC465Project\HomeSkilletBanner.JPG" alt="Home Skillet"> <!--Home Skillet Banner-->
+</a>	
+
+<div>
+<!--Begin Home Skillet Section-->
+
+
+	<h3>Hours</h3>
+		<img src="C:\Users\Joshua\Documents\Web Dev\COSC465Project\HomeSkilletHours.JPG" class="hours-pics" alt="Hours">
+	
+		<p class="special-p">Fulfill your soulfood cravings at Home Skillet</p>
+
+
+<hr/><!--Page Break-->
+
+
+<!--Begin Calica Section-->
+
+	<a href="C:\Users\Joshua\Documents\GitHub\BSUDiningPortal\Calica.html">
+		<img src="C:\Users\Joshua\Documents\Web Dev\COSC465Project\ChilacaBanner.JPG"  alt="Calica"> <!--Calica Banner-->
+	</a>
+	<h3>Hours</h3>
+		<img src="C:\Users\Joshua\Documents\Web Dev\COSC465Project\ChilacaHours.JPG" class="hours-pics" alt="Hours">
+</div>
+
+<hr/><!--Page Break-->
+
+
+
+
+<div>
+<!--Begin Bulldog Bites Section-->
+
+
+	<a href="C:\Users\Joshua\Documents\GitHub\BSUDiningPortal\BulldogBites.html"></a>
+		<img src="C:\Users\Joshua\Documents\Web Dev\COSC465Project\BulldogBitesBanner.JPG" alt="Bulldog Bites"> <!--Bulldog Bites Banner-->
+	</a>
+	<h3>Hours</h3>
+		<img src="C:\Users\Joshua\Documents\Web Dev\COSC465Project\BulldogBitesHours.JPG" class="hours-pics" alt="Hours">
+</div>	
+
+
+</body><!--End Body-->
+
+</html><!--End Page-->
+ 
